894a1b3ed4d96bad923a6fddda2e72097a9c38d8
Eduardo Bart <edub4rt@gmail.com>
2021-02-01 16:41:28 -0300

    Improve benchmarks

--- tests/benchmark.c
+++ tests/benchmark.c
@@ -6,20 +6,47 @@
 #include <x86intrin.h>
 mal_allocator allocator;
 
+#define N 10000000
+void* ptrs[N];
+
+void bench() {
+  int i;
+  unsigned long start, end;
+
+  start = __rdtsc();
+  for(i=0;i<N;++i) {
+    int size = i % 16;
+    ptrs[i] = mal_alloc(&allocator, size);
+  }
+  end = __rdtsc();
+  printf("alloc %lu\n", (end - start)/N);
+
+  start = __rdtsc();
+  for(i=0;i<N;++i) {
+    int size = i % 16;
+    ptrs[i] = mal_realloc(&allocator, ptrs[i], size*2, size);
+  }
+  end = __rdtsc();
+  printf("realloc %lu\n", (end - start)/N);
+
+  start = __rdtsc();
+  for(i=0;i<N;++i) {
+    mal_dealloc(&allocator, ptrs[i]);
+  }
+  end = __rdtsc();
+  printf("dealloc %lu\n", (end - start)/N);
+
+}
+
 int main(void) {
   mal_init(&allocator);
 
-  int i;
-  unsigned long start = __rdtsc();
-  for(i=0;i<1000000;++i) {
-    int size = i % 8;
-    void* ptr = mal_alloc(&allocator, size);
-    ptr = mal_realloc(&allocator, ptr, size*2, size);
-    ptr = mal_realloc(&allocator, ptr, size*4, size);
-    mal_dealloc(&allocator, ptr);
-  }
-  unsigned long end = __rdtsc();
-  printf("%lu\n", (end - start)/1000000);
+  printf("== warmup\n");
+  bench();
+  printf("== bench 1\n");
+  bench();
+  printf("== bench 2\n");
+  bench();
 
   mal_destroy(&allocator);
   return 0;
